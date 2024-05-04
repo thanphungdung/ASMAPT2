a9fd97d791812d0eba108b8a26345972f98560d4
thanphungdung <57036866+thanphungdung@users.noreply.github.com>
2024-05-04 22:47:05 +1000

    Add files via upload

---
+++ ftt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "LinkedList.h"
+
+/**
+ * manages the running of the program, initialises data structures, loads
+ * data, display the main menu, and handles the processing of options. 
+ * Make sure free memory and close all files before exiting the program.
+ **/
+int main(int argc, char **argv)
+{
+    /* validate command line arguments */
+    // TODO
+
+    std::cout << "Just a test, nothing implemented yet!" << std::endl;
+    
+    return EXIT_SUCCESS;
+}
